7845c51f0145b403d0c2f99251438c3ed4ab959f
i4things <45852539+i4things@users.noreply.github.com>
2019-01-27 14:02:26 +0000

    Smart Room/Office/House/Hotel Example

---
+++ examples/Feather32u4/SmartRoom/thing/thing_dht.h
@@ -0,0 +1,88 @@
+/**
+   USE OF THIS SOFTWARE IS GOVERNED BY THE TERMS AND CONDITIONS
+   OF THE LICENSE STATEMENT AND LIMITED WARRANTY FURNISHED WITH
+   THE PRODUCT.
+   <p/>
+   IN PARTICULAR, YOU WILL INDEMNIFY AND HOLD B2N LTD., ITS
+   RELATED COMPANIES AND ITS SUPPLIERS, HARMLESS FROM AND AGAINST ANY
+   CLAIMS OR LIABILITIES ARISING OUT OF THE USE, REPRODUCTION, OR
+   DISTRIBUTION OF YOUR PROGRAMS, INCLUDING ANY CLAIMS OR LIABILITIES
+   ARISING OUT OF OR RESULTING FROM THE USE, MODIFICATION, OR
+   DISTRIBUTION OF PROGRAMS OR FILES CREATED FROM, BASED ON, AND/OR
+   DERIVED FROM THIS SOURCE CODE FILE.
+*/
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// DHT ( Humidity/Temp)
+
+#include "DHT.h"
+
+#define DHT_PIN 6
+#define DHT_TYPE DHT22
+
+DHT dht(DHT_PIN, DHT_TYPE);
+
+//60sec 60 * 1000 (60000)
+#define DHT_TIMEOUT  10000
+uint32_t DHT_LAST_EXECUTE;
+
+float DHT_TEMP_VALUE;
+float DHT_HUM_VALUE;
+
+void DHT_INIT()
+{
+  //important make sure DHT is initialized
+
+  DHT_TEMP_VALUE = -20;
+  DHT_HUM_VALUE = 50;
+  dht.begin();
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+
+  if (isnan(h) || isnan(t))
+  {
+    // log error
+  }
+  else
+  {
+    DHT_TEMP_VALUE = t;
+    DHT_HUM_VALUE = (uint8_t) h;
+  }
+}
+
+void DHT_WORK()
+{
+  if (((uint32_t)(((uint32_t)millis()) - DHT_LAST_EXECUTE)) >= DHT_TIMEOUT)
+  {
+    DHT_LAST_EXECUTE = millis();
+
+    float h = dht.readHumidity();
+    float t = dht.readTemperature();
+    if ((isnan(h)) || (isnan(t)))
+    {
+     Serial.println("DHT read failed");
+    }
+    else
+    {
+
+      DHT_HUM_VALUE =  h;
+      DHT_TEMP_VALUE = t;
+   
+    }
+  }
+}
+
+uint8_t DHT_HUM_GET()
+{
+  return DHT_HUM_VALUE;
+}
+
+float DHT_TEMP_GET()
+{
+  return DHT_TEMP_VALUE;
+}
+
+
+
+
+
---
+++ examples/Feather32u4/SmartRoom/thing/thing_light.h
@@ -0,0 +1,55 @@
+/**
+   USE OF THIS SOFTWARE IS GOVERNED BY THE TERMS AND CONDITIONS
+   OF THE LICENSE STATEMENT AND LIMITED WARRANTY FURNISHED WITH
+   THE PRODUCT.
+   <p/>
+   IN PARTICULAR, YOU WILL INDEMNIFY AND HOLD B2N LTD., ITS
+   RELATED COMPANIES AND ITS SUPPLIERS, HARMLESS FROM AND AGAINST ANY
+   CLAIMS OR LIABILITIES ARISING OUT OF THE USE, REPRODUCTION, OR
+   DISTRIBUTION OF YOUR PROGRAMS, INCLUDING ANY CLAIMS OR LIABILITIES
+   ARISING OUT OF OR RESULTING FROM THE USE, MODIFICATION, OR
+   DISTRIBUTION OF PROGRAMS OR FILES CREATED FROM, BASED ON, AND/OR
+   DERIVED FROM THIS SOURCE CODE FILE.
+*/
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Door Lock
+
+
+#define LIGHT_PIN 10
+
+bool light_on = false;
+
+void LIGHT_INIT()
+{
+  pinMode(LIGHT_PIN, OUTPUT);
+}
+
+void LIGHT_ON()
+{
+  digitalWrite(LIGHT_PIN, HIGH);
+  
+  light_on = true;
+}
+
+
+void LIGHT_OFF()
+{
+  digitalWrite(LIGHT_PIN, LOW);
+
+  light_on = false;
+}
+
+
+void LIGHT_WORK()
+{
+  
+}
+
+bool IS_LIGHT_ON()
+{
+  return light_on;  
+}
+
+
+
